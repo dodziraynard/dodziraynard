bac3927692fee04861134851591ba9e11a7f3f81
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-07-10 18:28:02 +0000

    update

--- dodziraynard/settings/base.py
+++ dodziraynard/settings/base.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
