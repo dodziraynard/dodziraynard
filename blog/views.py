705fd08d9615a70a76d4f013d9a8288ed1aeddae
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-07-12 07:24:21 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.db.models import query
 from django.db.models.query_utils import Q
 from django.shortcuts import get_object_or_404, redirect, render
-from . models import Article
+from .models import Article
 
 
 def index(request):
@@ -10,10 +10,8 @@ def index(request):
     query = request.GET.get("query", "")
     if query:
         articles = Article.objects.filter(
-            Q(title__icontains = query) |
-            Q(tags__icontains = query) |
-            Q(text__icontains = query)
-        )
+            Q(title__icontains=query) | Q(tags__icontains=query)
+            | Q(text__icontains=query))
     else:
         articles = Article.objects.all()
 
@@ -21,10 +19,11 @@ def index(request):
         articles = articles.filter(published=True)
 
     context = {
-        "articles":articles.order_by("-created_at")[:20],
-        "query":query,
+        "articles": articles.order_by("-created_at")[:20],
+        "query": query,
     }
     return render(request, template_name, context)
+
 
 def store_article(request):
     if request.method == "POST":
@@ -34,12 +33,12 @@ def store_article(request):
 
         id = request.POST.get("id")
         if id:
-            Article.objects.filter(id=id).update(
-                title=title, text=text, published=published
-            )
+            Article.objects.filter(id=id).update(title=title,
+                                                 text=text,
+                                                 published=published)
         else:
             Article.objects.create(title=title, text=text, published=published)
-        
+
         return redirect("blog:index")
 
 
@@ -48,7 +47,7 @@ def edit_article(request, slug, article_id):
 
     template_name = 'blog/edit_article.html'
     context = {
-        "article":article,
+        "article": article,
     }
     return render(request, template_name, context)
 
@@ -56,13 +55,14 @@ def edit_article(request, slug, article_id):
 def article(request, slug, article_id):
     template_name = "blog/article.html"
     article = get_object_or_404(Article, id=article_id)
-    articles = Article.objects.filter(tags__in = article.tags.split()).exclude(id=article.id)
+    articles = Article.objects.filter(tags__in=article.tags.split()).exclude(
+        id=article.id)
 
     if not request.user.is_staff:
         articles = articles.filter(published=True)
 
     context = {
-        "article":article,
-        "articles":articles,
+        "article": article,
+        "articles": articles,
     }
     return render(request, template_name, context)
