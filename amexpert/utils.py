5af70e354c94946f633792c6b5fd4079a322833f
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2022-01-27 23:45:14 +0000

    fix: read secret keys from local_settings.py

--- amexpert/utils.py
+++ amexpert/utils.py
@@ -1,17 +1,16 @@
 import os
 from sendgrid.helpers.mail import Mail
 from sendgrid import SendGridAPIClient
-
+from django.conf import settings
 
 # update to your dynamic template id from the UI
 TEMPLATE_ID = 'd-57284dc5b6204b22b4e95d86075db174'
 
 # list of emails and preheader names, update with yours
 
+
 def SendDynamic(from_email, to_emails, fullname):
-    message = Mail(
-        from_email=from_email,
-        to_emails=to_emails)
+    message = Mail(from_email=from_email, to_emails=to_emails)
 
     # pass custom values for our HTML placeholders
     message.dynamic_template_data = {
@@ -20,8 +19,10 @@ def SendDynamic(from_email, to_emails, fullname):
     }
     message.template_id = TEMPLATE_ID
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY')) ## (^_^) This face is just to grab your attention for api key needed
+        sg = SendGridAPIClient(
+            settings.SENDGRID_API_KEY
+        )  ## (^_^) This face is just to grab your attention for api key needed
         response = sg.send(message)
+        return str(response.status_code)
     except Exception as e:
         print("Error: {0}".format(e))
-    return str(response.status_code)
